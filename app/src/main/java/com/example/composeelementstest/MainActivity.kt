e5d3c53297d147e6f4da8c0b3c0848b75080da25
Ehsan Taghavi <ehsantgv@gmail.com>
2020-10-22 23:46:19 +0330

    change text layout

--- app/src/main/java/com/example/composeelementstest/MainActivity.kt
+++ app/src/main/java/com/example/composeelementstest/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.composeelementstest
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import com.example.composeelementstest.ui.ComposeElementsTestTheme
 
@@ -17,22 +22,26 @@ class MainActivity : AppCompatActivity() {
             ComposeElementsTestTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    NewStory()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeElementsTestTheme {
-        Greeting("Android")
+        NewStory()
+    }
+}
+
+@Composable
+fun NewStory() {
+    Column() {
+        Text("Hello")
+        Text("How are you")
+        Text("This is declarative UI from Jetpack compose")
     }
 }
